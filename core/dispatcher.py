837684a9ee3543ba1b532c7aa73501608b64822c
RishiDargad18 <rishisdargad@gmail.com>
2026-04-01 01:12:08 +0530

    Remove unused problem imports from dispatcher.py

--- core/dispatcher.py
+++ core/dispatcher.py
@@ -1,10 +1,8 @@
-from problems import p1, p2, p3, p4, p5, p6
+from problems import p1, p2, p3, p4
 
 problem_map = {
     1: p1,
     2: p2,
     3: p3,
-    4: p4,
-    5: p5,
-    6: p6
+    4: p4
 }
