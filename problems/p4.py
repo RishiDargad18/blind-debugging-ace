0d3a87aee139324fc02cdd45a96f657f01db1b22
RishiDargad18 <rishisdargad@gmail.com>
2026-04-01 22:45:48 +0530

    Update p4.py

--- problems/p4.py
+++ problems/p4.py
@@ -1,14 +1,14 @@
-def run(user, cmd): 
+def run(user, cmd):
+    s = cmd.strip()
     n = len(s)
+
     s = list(s)
     res = [''] * n
 
-    # even indices → shift forward by 1
     evens = list(range(0, n, 2))
     for i in range(len(evens)):
         res[evens[i]] = s[evens[(i + 1) % len(evens)]]
 
-    # odd indices → shift forward by 1 (same step, separate cycle)
     odds = list(range(1, n, 2))
     for i in range(len(odds)):
         res[odds[i]] = s[odds[(i + 1) % len(odds)]]
