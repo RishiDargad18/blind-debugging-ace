7547f8138f20d7a1d3ba4f0d5ca1af8993c2d9d8
RishiDargad18 <rishisdargad@gmail.com>
2026-04-01 19:29:42 +0530

    Update run function to process string shifting
    Refactor run function to shift characters in a string based on even and odd indices.

--- problems/p4.py
+++ problems/p4.py
@@ -1,10 +1,17 @@
-def run(user, cmd):
-    if "count" not in user:
-        user["count"] = 0
+def run(user, cmd): 
+    n = len(s)
+    s = list(s)
+    res = [''] * n
 
-    user["count"] += 1
-    return int(cmd) * user["count"]
+    # even indices → shift forward by 1
+    evens = list(range(0, n, 2))
+    for i in range(len(evens)):
+        res[evens[i]] = s[evens[(i + 1) % len(evens)]]
 
-info = """Problem 4:
-Input: integer n
-"""
+    # odd indices → shift forward by 1 (same step, separate cycle)
+    odds = list(range(1, n, 2))
+    for i in range(len(odds)):
+        res[odds[i]] = s[odds[(i + 1) % len(odds)]]
+
+    return "".join(res)
+info = """Input: String s"""
